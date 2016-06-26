0a9239e48e57a9282e8bc0ce32b7ec4fae453263
goodly92@gmail.com <goodly92@gmail.com>
2016-06-26 13:02:32 +0300

    fix discovery content tests, added caching check

--- content/src/test/java/org/libme/content/service/ContentDiscoveryServiceTest.java
+++ content/src/test/java/org/libme/content/service/ContentDiscoveryServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.libme.content.client.ContentDiscoveryClient;
 import org.libme.content.domain.Film;
 import org.libme.content.domain.FilmDiscovery;
+import org.libme.content.repository.DiscoveryRepository;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
@@ -19,6 +20,9 @@ public class ContentDiscoveryServiceTest {
 
     @Mock
     private ContentDiscoveryClient discoveryClient;
+
+    @Mock
+    private DiscoveryRepository discoveryRepository;
 
     @InjectMocks
     private ContentDiscoveryService discoveryService;
@@ -35,7 +39,16 @@ public class ContentDiscoveryServiceTest {
     }
 
     @Test
-    public void discovery() throws Exception {
+    public void discoveryWithCache() throws Exception {
+        when(discoveryRepository.findByFilmID(kinopoiskId)).thenReturn(film);
+        Film discovery = discoveryService.discovery(kinopoiskId);
+        assertEquals(film.getNameRU(), discovery.getName());
+        assertEquals(film.getFilmID(), discovery.getKinopoiskId());
+    }
+
+    @Test
+    public void discoveryWithoutCache() throws Exception {
+        when(discoveryRepository.findByFilmID(kinopoiskId)).thenReturn(null);
         when(discoveryClient.getFilm(kinopoiskId)).thenReturn(film);
         Film discovery = discoveryService.discovery(kinopoiskId);
         assertEquals(film.getNameRU(), discovery.getName());
